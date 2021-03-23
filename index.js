a72b5ceb8fcb8df29b0fb66d37c0f423baa95fa3
choojiye <jiye_choo@tmax.co.kr>
2021-03-23 16:37:02 +0900

    [feat] 로그인 서비스
    - DB 연결
    - 키 관리

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express()
 const port = 5000
 
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
 mongoose.connect(
-    'mongodb+srv://choozii:1234@moviewebsite.indpp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  config.mongoURI,
     {
     useNewUrlParser:true, 
     useUnifiedTopology:true, 
@@ -15,7 +22,16 @@ mongoose.connect(
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!!!!! 무야호!!')
+  res.send('Hello World!!!!!!!! 무야호dndhdndh!!')
+})
+
+app.post('/register',(req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({success:true})
+  })
+
 })
 
 
