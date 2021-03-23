8078c772c19488bcbb85dbe9980854fd2046bf4b
choojiye <jiye_choo@tmax.co.kr>
2021-03-23 14:40:04 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose');
+mongoose.connect(
+    'mongodb+srv://choozii:1234@moviewebsite.indpp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+    useNewUrlParser:true, 
+    useUnifiedTopology:true, 
+    useCreateIndex:true, 
+    useFindAndModify:false
+    })
+    .then(()=>console.log('MongoDB connected....'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!!!!!!! 무야호!!')
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
