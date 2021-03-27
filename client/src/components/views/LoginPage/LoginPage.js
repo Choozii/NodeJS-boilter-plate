ad18ce5d2ba7c5def53de24e0db89517ab234452
Choozii <jiyechoo@gmail.com>
2021-03-28 00:56:59 +0900

    [feat] 로그인 페이지 프론트 구현

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,12 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../../../_actions/user_action'
 
 function Loginpage(props) {
+    const dispatch = useDispatch();
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
     
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        let body = {
+            email:Email,
+            password:Password
+        }
+        
+        dispatch(loginUser(body))
+        .then(response => {
+            console.log(response);
+            if(response.payload.loginSuccess){
+                props.history.push('/');
+            }
+        })
+    }
+
+    const styles={
+        page : {
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+            width : '100%',
+            height:'100vh'
+        },
+        forms:{
+            display:'flex',
+            flexDirection:'column'
+        }
+    }
 
     return (
-        <>
-            login page
-        </>
+        <div style={styles.page}>
+            <form style={styles.forms} onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button>Login</button>
+            </form>
+        </div>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import reduxThunk from 'redux-thunk';
+import rootReducer from './_reducers';
 
+const createStoreWithMiddleWare = applyMiddleware(promiseMiddleware, reduxThunk)(createStore);
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStoreWithMiddleWare(rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+     )}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
